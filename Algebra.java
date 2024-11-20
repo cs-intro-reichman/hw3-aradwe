961e20d15f25b3784515d2c49abb8882e3a3c766
Arad Weinstein <arad.weinstein@post.runi.ac.il>
2024-11-20 17:36:13 +0200

    version 1

--- Algebra.java
+++ Algebra.java
@@ -14,10 +14,12 @@ public class Algebra {
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
    		System.out.println(div(12,3));   // 12 / 3    
+		System.out.println(div(-12,3));   // -12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
    		System.out.println(mod(120,6));  // 120 % 6    
+		System.out.println(mod(17,3));  // 17 % 3    
    		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
@@ -25,43 +27,92 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for(int i = 0; i < x2; i++)
+		x1++;
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for(int i = 0; i < x2; i++)
+		x1--;
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int x3 = 0;
+		for(int i = 0; i < x2; i++)
+		x3 = plus(x3, x1);
+		return x3;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int x3 = x;
+		for(int i = 1; i < n; i++)
+		x3 = times(x3, x);
+		return x3;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int quotient = 0;
+		boolean negativeResult = false;
+		
+		// Prevent edge case of zero division
+		if (x2 == 0){
+			System.out.println("Devision by zero is not allowed!");
+			return 0;
+		}
+		
+		// Check negativity of the result and handle the numbers as positive for comfort
+		if(x1 < 0){
+			x1 = -x1;
+			if(x2 < 0){
+				x2 = -x2;
+			}
+			else{
+				negativeResult = true;
+			}
+		if(x2 < 0){
+			x2 = -x2;
+			negativeResult = true;
+		}
+		
+		// If x1 is greater than equal to x2 subtract x2 from x1 an increase the quotient by one.
+		} 
+		while (x1 >= x2) {
+			x1 = minus(x1, x2);
+			quotient++;
+		}
+		
+		// If negative result is true - make the quotient negative 
+		if (negativeResult) {
+			quotient = - quotient;
+		}
+		return quotient;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int divResult = div(x1, x2);
+		return minus(x1, times(x2, divResult));
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		// make sure the sqrt of the number is defined
+		if(x < 0){
+			System.out.println("Square root of a negative number is undefined");
+			return 0;
+		}
+		
+		//find the largest number whose square is less than or equal to x
+		int sqrtResult = 0;
+		while (times(sqrtResult, sqrtResult) <= x) {
+			sqrtResult++;
+		}
+		return minus(sqrtResult, 1);
 	}	  	  
 }
